5bc2e7fdfe0bf857bad37eddd236e173d1b9895e
Sylvia <a00825972@tec.mx>
2023-12-08 19:59:18 -0600

    Parentheses.cpp

--- Parentheses.cpp
+++ Parentheses.cpp
@@ -1,1 +1,25 @@
-//Valid parentheses sol
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> parenthesesStack;
+        unordered_map<char, char> closingToOpening {
+            {')', '('},
+            {'}', '{'},
+            {']', '['}
+        };
+
+        for (char c : s) {
+            if (closingToOpening.find(c) != closingToOpening.end()) {
+                if (parenthesesStack.empty() || parenthesesStack.top() != closingToOpening[c]) {
+                    return false;  
+                }
+
+                parenthesesStack.pop();
+            } else {
+                parenthesesStack.push(c);
+            }
+        }
+
+        return parenthesesStack.empty();
+    }
+};

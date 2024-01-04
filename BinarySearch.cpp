86639b7f39c77ac49ec659c6b434dd2884fa946d
Sylvia <A00825972@tec.mx>
2024-01-04 13:36:29 -0600

    BinarySearch Solution

---
+++ BinarySearch.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    //Literally binarySearch
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        int mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            // cout << mid << endl;
+            if (nums[mid] == target) {
+                return mid;
+            }
+    
+            if (nums[mid] < target) {
+                left = mid + 1;
+            }
+    
+            else {
+                right = mid - 1;
+            }
+        }
+        return left;
+        }
+};

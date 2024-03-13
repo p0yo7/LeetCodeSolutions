6eb884799a8c1f30ca289f15c85b3b8c72cd952f
Cortes Oscar <cortes.o.inter@e80group.com>
2024-03-13 16:07:12 -0600

    p287 with unordered map

--- Algorithms/p287.cpp
+++ Algorithms/p287.cpp
@@ -1,3 +1,4 @@
+// Using unordered set min 129 ms
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
@@ -5,6 +6,18 @@ public:
         for (int i = 0; i < nums.size(); i++){
             if (set.find(nums[i]) != set.end()) return nums[i];
             set.insert(nums[i]);
+        }
+        return -1;
+    }
+};
+// Using unordered map min 126 ms
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        unordered_map<int,int> map;
+        for (int i = 0; i < nums.size(); i++){
+            map[nums[i]]++;
+            if(map[nums[i]] > 1) return nums[i];
         }
         return -1;
     }

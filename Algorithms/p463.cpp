a1f1644c4e7f954f4d811cd83d8da4d8e5f965c2
Sylvia Cortes <cortes.o.inter@e80group.com>
2024-04-18 08:10:25 -0600

    p463 solution

---
+++ Algorithms/p463.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int i = 0, sum = 0;
+        for(; i < grid.size(); i++){
+            for(int j = 0; j < grid[0].size(); j++){
+                if (grid[i][j] == 1){
+                    sum += 4;
+                    if (i > 0 && grid[i - 1][j] == 1) sum -= 2;
+                    if (j > 0 && grid[i][j - 1] == 1) sum -= 2;
+                }
+            }
+        }
+        return sum;
+    }
+};

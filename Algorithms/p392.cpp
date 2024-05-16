f09f6dd2bbc6458686eba5250e1bc32184eb9605
Sylvia Cortes <cortes.o.inter@e80group.com>
2024-05-16 09:07:55 -0600

    p392 solution

--- Algorithms/p392.cpp
+++ Algorithms/p392.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
+        int i = 0;
         int j = 0;
-        for(int i = 0; i < t.size(); i++){
-            if (s[j] == t[i]) j++;
+        while (i < s.size() && j < t.size()){
+            if (s[i] == t[j]){
+                i++;
+            }
+            j++;
         }
-        return j == s.size();
+        return i == s.size();
     }
 };
